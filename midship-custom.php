b7a4f4cc297ccb40876cb67fa020a9a3bd32edf9
Steven K Word <stevenword@gmail.com>
2015-06-16 07:46:07 -0700

    test

--- midship-custom.php
+++ midship-custom.php
@@ -175,7 +175,7 @@ function midship_accredit_source( $content ) {
 	$link  = get_post_meta( $post->ID, 'linkSourceURL', true );
 
 	if( $link ) {
-		$content = '<p class="post-byline">' . the_author_posts_link() .' at <a target="_blank" href="' . esc_url($link) . '">' . esc_html($title) . '</a>' . '</p>' . $content; //. ' &middot; ' . the_time(get_option('date_format'))
+		$content = '<p class="post-byline">' . get_the_author_link() .' at <a target="_blank" href="' . esc_url($link) . '">' . esc_html($title) . '</a>' . '</p>' . $content; //. ' &middot; ' . the_time(get_option('date_format'))
 		return $content;
 	}
 }
