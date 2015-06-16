224387d1864e2db689569579afaab9e069ad4469
Steven K Word <stevenword@gmail.com>
2015-06-16 11:44:04 -0700

    consolidate headers and footers

--- midship-custom.php
+++ midship-custom.php
@@ -242,6 +242,8 @@ function midship_render_content_header( $content ){
 
 	$new_content = midship_get_singular_byline();
 
+	wp_link_pages(array('before' => '<div class="pagination">', 'after' => '</div>', 'link_before'  => '<span class="current"><span class="currenttext">', 'link_after' => '</span></span>', 'next_or_number' => 'next_and_number', 'nextpagelink' => __('Next','mythemeshop'), 'previouspagelink' => __('Previous','mythemeshop'), 'pagelink' => '%','echo' => 1 ));
+
 	return $new_content . $content;
 
 }
