e0bdd89c3847af90eed37b79be478e0e066cfe17
Steven K Word <stevenword@gmail.com>
2015-06-16 09:10:44 -0700

    single byline

--- midship-custom.php
+++ midship-custom.php
@@ -200,6 +200,7 @@ function midship_accredit_source( $content ) {
 		$content .= '<p>Special Thanks to <a target="_blank" href="' . esc_url($link) . '">' . esc_html($title) . '</a></p>' . $content;
 		return $content;
 	}
+	return $content;
 }
 add_filter( 'the_content', 'midship_accredit_source', 11 );
 
